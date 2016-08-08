9463e0bc1298dd780cd281fed200ede624b9b6d7
Saurabh Ghewari <saurabh.ghewari@selenite.co>
2016-08-08 17:41:37 +0530

    updated the listening port to 8000.

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ app.use('/', function(req, res){
 	res.send("Hello World, Successfully deployed app on Amazon.");
 });
 
-app.listen(3000, function(){
-	console.log("server listening on Port:-3000");
+app.listen(8000, function(){
+	console.log("server listening on Port:-8000");
 });
